6b145b1361df8ccee2bf7b7ef38104d887252ee4
Alex Groza <72754857+Chamosiala@users.noreply.github.com>
2020-10-18 00:04:58 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,57 +1,49 @@
-board = []
-turns = 0
-for i in range(3):
-    board.append([])
-    for j in range(3):
-        board[i].append(' ')
+import random
 
-print(f"""---------
-| {board[0][0]} {board[0][1]} {board[0][2]} |
-| {board[1][0]} {board[1][1]} {board[1][2]} |
-| {board[2][0]} {board[2][1]} {board[2][2]} |
----------""")
-
+print("H A N G M A N")
+words = ['python', 'java', 'kotlin', 'javascript']
 while 1:
-    while 1:
-        coords = input("Enter the coordinates: ").split()
-        x = coords[0]
-        y = coords[1]
-        if not x.isalnum() or not y.isalnum():
-            print("You should enter numbers!")
-        elif int(x) > 3 or int(x) < 1 or int(y) > 3 or int(y) < 1:
-            print("Coordinates should be from 1 to 3!")
-        elif len(coords) > 2:
-            print("You should only enter 2 numbers!")
-        elif board[abs(int(y) - 3)][int(x) - 1] != ' ':
-            print("This cell is occupied! Choose another one!")
-        else:
-            if turns % 2:
-                board[abs(int(y) - 3)][int(x) - 1] = 'O'
+    choice = input("Type \"play\" to play the game, \"exit\" to quit: ")
+    while choice != "play" and choice != "exit":
+        choice = input("Type \"play\" to play the game, \"exit\" to quit: ")
+    if choice == "exit":
+        break
+    correct_word = random.choice(words)
+    letters = set()
+    for char in correct_word:
+        letters.update(char)
+    hidden_word = '-' * (len(correct_word))
+    hidden_word = list(hidden_word)
+    guessed_letters = set()
+
+    tries = 8
+    while tries > 0:
+        print("")
+        for i in range(len(hidden_word)):
+            print(hidden_word[i], end='')
+        letter = input("\nInput a letter: ")
+
+        if letter.islower() and len(letter) == 1 and letter not in guessed_letters:
+            if letter in letters:
+                guessed_letters.update(letter)
+                for i in range(len(hidden_word)):
+                    if correct_word[i] == letter:
+                        hidden_word[i] = letter
             else:
-                board[abs(int(y) - 3)][int(x) - 1] = 'X'
-            turns += 1
-            break
+                print("No such letter in the word")
+                guessed_letters.update(letter)
+                tries -= 1
+            if '-' not in hidden_word:
+                break
+        elif len(letter) != 1:
+            print("You should input a single letter")
+        elif letter.isupper() or not letter.isalpha():
+            print("It is not an ASCII lowercase letter")
+        else:
+            print("You already typed this letter")
 
-    print(f"""---------
-| {board[0][0]} {board[0][1]} {board[0][2]} |
-| {board[1][0]} {board[1][1]} {board[1][2]} |
-| {board[2][0]} {board[2][1]} {board[2][2]} |
----------""")
-
-    if (['X', 'X', 'X'] in board or board[0][0] == board[1][1] == board[2][2] == 'X'
-            or board[0][2] == board[1][1] == board[2][0] == 'X'
-            or board[0][0] == board[1][0] == board[2][0] == 'X'
-            or board[0][1] == board[1][1] == board[2][1] == 'X'
-            or board[0][2] == board[1][2] == board[2][2] == 'X'):
-        print("X wins")
-        break
-    elif (['O', 'O', 'O'] in board or board[0][0] == board[1][1] == board[2][2] == 'O'
-          or board[0][2] == board[1][1] == board[2][0] == 'O'
-          or board[0][0] == board[1][0] == board[2][0] == 'O'
-          or board[0][1] == board[1][1] == board[2][1] == 'O'
-          or board[0][2] == board[1][2] == board[2][2] == 'O'):
-        print("O wins")
-        break
-    elif turns == 9:
-        print("Draw")
-        break
+    if tries > 0:
+        print("You guessed the word!")
+        print("You survived!\n")
+    else:
+        print("You lost!\n")
